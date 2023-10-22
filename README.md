323bebd6797a975c87087f31e62b022831a8025a
aheath <aaron.heath5447@gmail.com>
2023-10-22 14:50:22 -0400

    Corrected script error for DOM traversal.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# workday-calendar
+# Workday Calendar
+## Description
+This application allows the user to store and review their events in a given day. Between regular business hours (9am-5pm), the user can store information about events that occurs in each time block. For user convenience, time blocks are highlighted different colors relative to the current time: grey for past events, green for upcoming, and red for current events. The application can be found [here](https://aaron-heath.github.io/password-generator/).
+
+The application will validate user input to ensure that it conforms to the variable types and parameters outlined in the prompts provided to the user.
+
+See the a screenshot below:
+
+![Password Generator Screenshot](./password_generator_screenshot.png)
+
+## Installation
+
+N/A
+
+## Usage
+
+Press the 'Generate Password' button to begin the prompts. The password length must be between 8 and 128 characters. If the user enters a length outside of those parameters or enters something other than a number, they will be prompted that the input is invalid. If the user selects no criteria, they will be prompted to criteria were selected and the function will return.
+
+## Credits
+
+N/A
--- script.js
+++ script.js
@@ -2,35 +2,35 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  var $timeBlocks = $("body").children("div").children("div");
+  var $mainContent = $("body").children("div")
+  var $timeBlocks = $mainContent.children("div");
   var $currentDay = $("#currentDay");
 
   var now = dayjs();
   var militaryHour = now.format("H")
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+
+$timeBlocks.on("click", "button", function(event) {
+  let $parentBlock = $(event.target).parent()//.parent();
+  console.log($parentBlock)
+  let elId = $parentBlock.attr('id');
+  console.log(elId);
+  let elInput = $parentBlock.children("textarea").val()
+  console.log(elInput);
+  localStorage.setItem(elId,elInput);
+
+})
+
   function renderBlocks() {
     for (let $child of $timeBlocks) {
-
-      // TODO: Add code to get any user input that was saved in localStorage and set
-      // the values of the corresponding textarea elements. HINT: How can the id
-      // attribute of each time-block be used to do this?
       // Get information from local storage if it exists
       let storedData = localStorage.getItem($child.id);
       if(storedData) {
-        console.log(storedData);
         $($child).children("textarea").val(storedData);
       }
       
 
 
       let childHour = $child.id.split("-")[1];
-      console.log(childHour);
 
       // Flow control: Add class past, present, or future based on the childElement's ID.
       if(parseInt(childHour) < parseInt(militaryHour)) {
